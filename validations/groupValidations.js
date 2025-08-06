673e8663a460d597e1502d5e07142be486031b78
Prakanksh <nagpalprakankshabvpy@gmail.com>
2025-08-06 15:52:40 +0530

    allow space and underscore in group

--- validations/groupValidations.js
+++ validations/groupValidations.js
@@ -11,11 +11,11 @@ const titleValidation = Joi.string()
 
 const createGroup = Joi.object({
   title: Joi.string()
-    .pattern(/^[a-zA-Z0-9_]+$/)
+    .pattern(/^[a-zA-Z0-9_ ]+$/)
     .min(3)
     .optional()
     .messages({
-      "string.pattern.base": "Title can only contain letters, numbers, and underscores",
+      "string.pattern.base": "Title can only contain letters, numbers, underscores, and spaces",
       "string.base": "Title must be a string",
       "string.min": "Title must be at least 3 characters long",
     }),
@@ -26,11 +26,11 @@ const createGroup = Joi.object({
 
 const updateGroup = Joi.object({
   title: Joi.string()
-    .pattern(/^[a-zA-Z0-9_]+$/)
+    .pattern(/^[a-zA-Z0-9_ ]+$/)
     .min(3)
     .optional()
     .messages({
-      "string.pattern.base": "Title can only contain letters, numbers, and underscores",
+      "string.pattern.base": "Title can only contain letters, numbers, underscores, and spaces",
       "string.base": "Title must be a string",
       "string.min": "Title must be at least 3 characters long",
     }),
